fecc977f00e6179cbc64f5535492a6be4d921a7f
huge <learnitjs162@.com>
2017-09-14 20:57:17 +0800

    我在添加到暂存之前,忽略了bin,setting文件

---
+++ testgit/src/com/heima/git/Tgit.java
@@ -0,0 +1,13 @@
+package com.heima.git;
+
+public class Tgit {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("首先将要忽略的文件 ,忽略掉  例如  bin, settiing");
+		System.out.println("再将文件添加到暂存区");
+		System.out.println("再来一步才是提交 到仓库");
+		
+	}
+}
